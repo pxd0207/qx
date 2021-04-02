00c0365e94632ede26cfab0e55a5c1d0e9040425
pxd0207 <55773646+pxd0207@users.noreply.github.com>
2021-04-02 21:43:13 +0800

    Create MolyCam and VaporCam.js

---
+++ MolyCam and VaporCam.js
@@ -0,0 +1,13 @@
+[rewrite_local]
+
+# > MolyCam and VaporCam 拍出你的美 解锁VIP（20210402）
+
+https:\/\/pay\.wecut\.com\/apple\/(iosVerifyReceipt|iosAppVerifyReceipt)\.php url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/mv.js
+
+
+
+[mitm]
+
+hostname = 
+
+pay.wecut.com
